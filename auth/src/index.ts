4f982213b2921f6268a06b43cb1eb11861ca4be8
Ifeanyi Ofoefule <ofoefulec_fny@yahoo.com>
2022-01-12 07:24:38 -0800

    kubernetes and  skaffold setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hello ha!!');
+});
+
 app.listen(3000, () => {
-    console.log('Listening at port 3000');
+    console.log('Listening at port 3000!');
 });
