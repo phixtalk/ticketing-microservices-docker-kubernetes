0586898024724665b27ff145fcd9a17f721165dc
Ifeanyi Ofoefule <ofoefulec_fny@yahoo.com>
2022-01-13 04:16:56 -0800

    implemented custom error handling feature

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import 'express-async-errors';//makes sure we don't have to call next() 
+//in error handler when throwing errors in async functions
 import { json } from 'body-parser';
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,6 +17,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async () => {
+    throw new NotFoundError();
+})
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('Listening at port 3000!');
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,13 +1,24 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
-], (res: Request, req: Response) => {
-    req.send('Hi there!');
+], (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+    }
+
+    throw new DatabaseConnectionError();
+    const { email, password } = req.body;
+
+    res.send({});
 });
 
 export { router as signupRouter }
