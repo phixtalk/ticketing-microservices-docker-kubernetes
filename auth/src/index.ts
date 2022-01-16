1f6c18236dedbd827f46d69e873b82dc963642f4
Ifeanyi Ofoefule <ofoefulec_fny@yahoo.com>
2022-01-16 04:31:29 -0800

    implemented jwt authentication, updated start function to check if env variables are set before running containers

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';//makes sure we don't have to call next()
 //in error handler when throwing errors in async functions
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -12,7 +13,16 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set('trust proxy', true);//because traffic is been proxied to our app via
+//ingress nginx, this make express to be aware that it's behind a proxy, and it's
+//ok to trust traffic from the proxy
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,//disable encryption, because json is already encrypted
+        secure: true//allows cookies to be used only on https connection
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -26,6 +36,10 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
         console.log("Connected to MongoDB");
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 import { User } from '../models/user';
 import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
+
 
 const router = express.Router();
 
@@ -27,7 +29,17 @@ router.post('/api/users/signup', [
     const user = User.build({ email, password });
     await user.save();
     console.log("New user created");
-    
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+
+    // Store it on session object
+    req.session = {
+        jwt: userJwt
+    };
 
     res.status(201).send(user);
 });
